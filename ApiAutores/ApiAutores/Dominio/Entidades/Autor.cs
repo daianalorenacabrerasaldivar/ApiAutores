9887bb13c076e139920808499070f9e91b6de54c
Daiana Lorena Cabrera Saldivar <iodenteken@hotmail.com>
2023-03-16 22:01:06 -0300

    se intalo entity framework core

--- ApiAutores/ApiAutores/Dominio/Entidades/Autor.cs
+++ ApiAutores/ApiAutores/Dominio/Entidades/Autor.cs
@@ -3,6 +3,6 @@
     public class Autor
     {
         public int Id { get; set; }
-        public string Nombre { get; set;}
+        public string Nombre { get; set; }
     }
 }
---
+++ ApiAutores/ApiAutores/Infraestructura/Persistencia/AppicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Dominio.Entidades;
+
+namespace ApiAutores.Infraestructura.Persistencia
+{
+    public class AppicationDbContext : DbContext
+    {
+        public AppicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+
+    }
+}
