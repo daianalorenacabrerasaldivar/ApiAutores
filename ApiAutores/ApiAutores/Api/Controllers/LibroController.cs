921a7d0457d39931bef30fa04e6820e0d8cba110
Daiana Lorena Cabrera Saldivar <iodenteken@hotmail.com>
2023-10-09 09:56:45 -0300

    ControllerLibro agregate method post and get

--- ApiAutores/ApiAutores/Api/Controllers/LibroController.cs
+++ ApiAutores/ApiAutores/Api/Controllers/LibroController.cs
@@ -1,11 +1,37 @@
-using Microsoft.AspNetCore.MVC;
+using ApiAutores.Dominio.Entidades;
+using ApiAutores.Infraestructura.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace WebApiAutores.Controllers{
+namespace WebApiAutores.Api.Controllers
+{
+    [ApiController]
+    [Route("api/libros")]
+    public class LibrosControllers : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public LibrosControllers(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Libro>> Get(int id)
+        {
+            return await _context.Libros.FirstOrDefaultAsync(x => x.Id == id);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post(Libro libro)
+        {
+            var existeAutor= await _context.Libros.AnyAsync(x=> x.Id == libro.Id);
+            if (!existeAutor)
+            {
+                return BadRequest($"No existe el autor de Id {libro.AutorId}");
 
-[ApiController]
-[Route("api/libros")]
+            }
+            _context.Add(libro);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
 
-public class LibrosControllers: ControllerBase{
-
-
+    }
 }
